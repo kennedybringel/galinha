465d5709fbda81d4b20e146259bc2728aafd0599
kennedy <kken4229@gmail.com>
2024-05-21 02:03:26 -0300

    Adicionando imagem de fundo fixa

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bem-vindo ao Meu Site Simples</h1>
-        <p>
-          Este é um exemplo de um site React simples.
-        </p>
+        <h1>Título</h1>
+        <p>Este é o primeiro texto.</p>
+        <button>Botão</button>
+        <p>Este é o segundo texto.</p>
+        <p>Este é o terceiro texto.</p>
       </header>
     </div>
   );
